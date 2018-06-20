8791be2a5d00a792986ed595699e5146516ab0e1
liyan0411 <541267924@qq.com>
2018-06-20 13:16:35 +0800

    来货备案，菜单配置

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,7 +4,38 @@ const app = getApp()
 
 Page({
   data: {
-    
+    listData:[
+      {
+        name:"大白菜",
+        num:"500KG",
+        time:"06.01 07:00",
+        status:0
+      },
+      {
+        name: "苹果",
+        num: "400KG",
+        time: "06.01 05:00",
+        status: 1
+      },
+      {
+        name: "茄子",
+        num: "300KG",
+        time: "06.01 07:20",
+        status: 2
+      },
+      {
+        name: "苹果",
+        num: "400KG",
+        time: "06.01 05:00",
+        status: 1
+      },
+      {
+        name: "香蕉",
+        num: "250KG",
+        time: "06.01 07:20",
+        status: 1
+      }
+    ]
   },
   onLoad: function () {
 
