9dca02570844ddddada8b96af3ca179580466902
unknown <shaoliwei465@PAICMA.LOCAL>
2018-06-20 16:51:27 +0800

    时长管理完成一部分，没有背景图&&标注

--- src/pages/marketManagement/marketManagement.js
+++ src/pages/marketManagement/marketManagement.js
@@ -5,62 +5,78 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    isShowPayTheFees: true,
+    isShowPayRecord: false
   },
-
+  toggle(e) {
+    let type = e.currentTarget.dataset.type
+    console.log(type)
+    if (type == 1) {
+      this.setData({
+        isShowPayTheFees: true,
+        isShowPayRecord: false
+      })
+    }
+    if (type == 2) {
+      this.setData({
+        isShowPayTheFees: false,
+        isShowPayRecord: true
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
